90392293e08aab265984edc0e33d84bbcfc56d2b
SuperSo6 <89916346+SuperSo6@users.noreply.github.com>
2022-01-19 17:24:10 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sqlite3
 import time
 from turtle import st
-from update_db import  update_csv, update_db_cours
+import update_db
 import web_scrapping
 
 def start(value):
@@ -10,7 +10,9 @@ def start(value):
                      'grt':'the-graph',
                      'xlm':'stellar',
                      'mana':'decentraland',
-                     'near':'near-protocol'}
+                     'near':'near-protocol',
+                     'fantom':'fantom',
+                     'cop':'copiosa-coin'}
     if value == 1:
         name = input('Name of crypto searched : ').lower()
         
@@ -18,16 +20,17 @@ def start(value):
             if nom==name:
                 link = Dico_Link[nom]
         prix = web_scrapping.recherche(link)
-        print(f"{name.upper()} value is currently : {prix[1:]} €")    
+        print(f"{name.upper()} current value is : {prix[1:]} €")    
         valeur= prix[1:]
         valeur = float(valeur)
 
        
     elif value == 2 :
-        pass
+        update_db.add_invest()
 
     elif value == 3:
-        update_db_cours()
+        update_db.update_db_cours()
+        print('Database Uploaded !')
         
 
 
@@ -36,17 +39,22 @@ def start(value):
         
 
 
+
+ 
+
+  
+       
 
 
 
 if __name__=='__main__':
-    print( '_________                        __       ____   ____      .__')                 
-    print( '\_   ___ \_______ ___.__._______/  |_  ___\   \ /   /____  |  |  __ __   ____')  
-    print( '/    \  \/\_  __ <   |  |\____ \   __\/  _ \   Y   /\__  \ |  | |  |  \_/ __ \ ')
-    print( '\     \____|  | \/\___  ||  |_> >  | (  <_> )     /  / __ \|  |_|  |  /\  ___/') 
-    print( ' \______  /|__|   / ____||   __/|__|  \____/ \___/  (____  /____/____/  \___  >')
-    print( '        \/        \/     |__|                            \/                 \/ ')
-    Liste_Actions = ['-1 looking for price of crypto in euro','-2 add an investisment in your DataBase','-3 update your DB']
+    print( '   ___                 _                                                 ')                 
+    print( '  / __\ __ _   _ _ __ | |_ ___   /\/\   __ _ _ __   __ _  __ _  ___ _ __ ')  
+    print( " / / | '__| | | | '_ \| __/ _ \ /    \ / _` | '_ \ / _` |/ _` |/ _ \ '__|")
+    print( '/ /__| |  | |_| | |_) | || (_) / /\/\ \ (_| | | | | (_| | (_| |  __/ |   ') 
+    print( '\____/_|   \__, | .__/ \__\___/\/    \/\__,_|_| |_|\__,_|\__, |\___|_| ')
+    print( '           |___/|_|                                      |___/    ')
+    Liste_Actions = ['-1 search price of crypto in euro','-2 add an investisment in your DataBase','-3 auto-update your DB ']
     for i in Liste_Actions:
         print(i)
     val = int(input('Enter a number : '))
--- update_db.py
+++ update_db.py
@@ -2,18 +2,32 @@ from asyncio import current_task
 import csv
 from email import header
 from email.utils import localtime
+from lib2to3.pgen2 import token
+from lib2to3.pgen2.token import tok_name
 import re
 import sqlite3
 import os
+from telnetlib import GA
 from tkinter.messagebox import NO
 import time
 
 import web_scrapping
 
+global Dico_Link
+Dico_Link = {'amp':'amp',
+                     'xrp':'xrp',
+                     'grt':'the-graph',
+                     'xlm':'stellar',
+                     'mana':'decentraland',
+                     'near':'near-protocol',
+                     'fantom':'fantom',
+                     'cop':'copiosa-coin'}
+
+
 def update_csv():
     con = sqlite3.connect('Crypto.db')
     cursor = con.cursor()
-    cursor.execute("select Invest.Nom_Crypto,Argent_Investit,Nombres_Token,Date,Cours_Euro  from Invest,Cours_Monnaie where Invest.Nom_Crypto=Cours_Monnaie.Nom_Crypto ;")
+    cursor.execute("select Invest.Nom_Crypto,Argent_Investit,Nombres_Token,Date,Cours_Euro,Valeur,Gain,Pertes  from Invest,Cours_Monnaie where Invest.Nom_Crypto=Cours_Monnaie.Nom_Crypto ;")
     with open("Crypto.csv", "w") as csv_file:
       csv_writer = csv.writer(csv_file, delimiter=";")
       csv_writer.writerow([i[0] for i in cursor.description])
@@ -25,12 +39,7 @@ def update_csv():
     con.close()
 
 def update_db_cours():
-    Dico_Link = {'amp':'amp',
-                     'xrp':'xrp',
-                     'grt':'the-graph',
-                     'xlm':'stellar',
-                     'mana':'decentraland',
-                     'near':'near-protocol'}
+    
     
     con = sqlite3.connect('Crypto.db')
     cur = con.cursor()
@@ -53,9 +62,38 @@ def update_db_cours():
         temps = time.asctime(time.localtime())
         cur.execute(f'UPDATE Cours_Monnaie SET Cours_Euro = {element[1]}, Date = "{temps}" WHERE Nom_Crypto="{element[0]}"; ')
         con.commit()    
-   
+
     con.close()
 
 
+def add_invest():
+    con = sqlite3.connect('Crypto.db')
+    cur = con.cursor()
 
-update_db_cours()
+    Nom_Crypto = input('Name of crypto : ')
+    Amount = float(input('Amount invested in : '))
+    Date = input('Enter the date  DD.MM.YYYY (Like that -> 15.03.2022 ) : ')
+    Token_Achat = float(input('Amount of tokens bought'))
+    for nom in Dico_Link:
+            
+            if nom==Nom_Crypto.lower():
+                link= Dico_Link[nom]
+                prix = web_scrapping.recherche(link)
+    prix_actuel = float(prix[1:])
+            
+    ValeurAchat = Amount/Token_Achat
+    Gain = ' + 0 %'
+    Pertes = ' - 0 % '
+    
+
+    cur.execute(f"SELECT * From Invest where Nom_Crypto='{Nom_Crypto.upper()}';")
+    if cur.fetchall() == [] : 
+        cur.execute(f"Insert into Invest Values ('{Nom_Crypto.upper()}',{Amount},{Token_Achat},{ValeurAchat},'{Date}',{prix_actuel},'{Gain}','{Pertes}');")
+        con.commit()
+    con.close()
+
+
+add_invest()
+    
+
+
